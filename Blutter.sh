d03d3db2d1ca526003769647eb2fd19aaebf7781
AKASH HASAN <akashhasan66328@gmail.com>
2026-03-31 11:25:35 +0600

    Update Blutter.sh

--- Blutter.sh
+++ Blutter.sh
@@ -1,12 +1,17 @@
 clear
 termux-setup-storage
-apt update -y && apt upgrade -y
-pkg install -y git cmake ninja build-essential pkg-config libicu capstone fmt python-pip wget zip ripgrep neovim
-pip install requests pyelftools pycryptodome
+
+export DEBIAN_FRONTEND=noninteractive
+apt update -y && apt upgrade -y -o Dpkg::Options::="--force-confold"
+
+pkg install -y git cmake ninja build-essential pkg-config libicu capstone fmt python wget zip ripgrep neovim
+
+pip install --upgrade pip
+pip install --no-cache-dir requests pyelftools pycryptodome
+
 wget https://github.com/dedshit/blutter-termux/archive/refs/heads/main.zip
-unzip main.zip && cd blutter-termux-main && zip -r main.zip . && mv main.zip $HOME && cd $HOME && unzip main.zip && rm -rf blutter-termux-main && rm main.zip
-cd && clear
-termux-open-url https://t.me/akash_black_fox
+unzip main.zip
+cd blutter-termux-main
 
 RED="\e[31m"
 PINK="\e[95m"
@@ -17,4 +22,6 @@ echo -e "${GREEN}Owner AKASH HASAN${ENDCOLOR}"
 echo -e "${GREEN}TG @black_fox_teach${ENDCOLOR}"
 echo -e "${GREEN}GitHub akash-black-fox${ENDCOLOR}"
 
-echo -e "\e[32m===============================================\e[0m" && echo -e "\e[1;34m[*] \e[32mGreat ! \e[1;31mB(l)utter \e[32mInstalled Successfully..\e[0m" && echo -e "\e[32m===============================================\e[0m"
+echo -e "\e[32m=======================================\e[0m"
+echo -e "\e[1;34m[*] \e[32mBlutter Installed Successfully ✔\e[0m"
+echo -e "\e[32m=======================================\e[0m"
